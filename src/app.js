645a41fff79e79826ed337d995ccf9bbe6397e94
anastasia <anastasiaverbenets@gmail.com>
2024-07-14 16:26:17 +0300

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
 import { composers } from "./composersData";
 import itemsTemplate from "../templates/card.hbs"
-import Handlebars from "handlebars";
-
-// const template = Handlebars.compile("Name: {{name}}");
-// console.log(template({ name: "Nils" }));
 
 const list = document.querySelector('.composer__list');
 
